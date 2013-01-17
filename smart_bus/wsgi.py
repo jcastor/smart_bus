426d84fb3f058777d5ac7b7317c7eb18e8d736fe
Jason Castor <jason@jason-castor.com>
2013-01-17 11:05:36 -0800

    modified wsgi.py to get webserver running, enabled admin site

--- smart_bus/wsgi.py
+++ smart_bus/wsgi.py
@@ -1,28 +1,24 @@
-"""
-WSGI config for smart_bus project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
+ALLDIRS = ['/home/jcastor/.virtualenvs/499project/lib/python2.6/site-packages']
+# the above directory depends on the location of your python installation.
+# if using virtualenv, it will need to match your projects locale.
 import os
+import sys
+import site
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smart_bus.settings")
+prev_sys_path = list(sys.path)
+for directory in ALLDIRS:
+    site.addsitedir(directory)
+    new_sys_path = []
+    for item in list(sys.path):
+        if item not in prev_sys_path:
+            new_sys_path.append(item)
+            sys.path.remove(item)
+            sys.path[:0] = new_sys_path
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+    # change this depending on your project.
+sys.path.append('/home/jcastor/django/499project/smart_bus')
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+os.environ['PYTHON_EGG_CACHE'] = '/home/jcastor/.python-eggs'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'smart_bus.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()                                                            
