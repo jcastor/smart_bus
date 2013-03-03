30e275edee01f680001839392271c6fa7753e378
Jason Castor <jason@jason-castor.com>
2013-03-02 22:46:31 -0800

    fixed some time load stuff

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -6,10 +6,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'smart_bus.settings'
 from bus_tracker.models import BusRoute, BusStop, Direction, ArrivalTime, Bus, BusLocation, Light
 import csv, re
 
-data1 = open(sys.argv[1])
-data2 = open(sys.argv[2])
+data2 = open(sys.argv[1])
 
-cr = csv.reader(data1)
 cr2 = csv.reader(data2)
 
 eastpattern = r'^(eastbound)'
@@ -24,11 +22,6 @@ westregex = re.compile(westpattern, re.IGNORECASE)
 northregex = re.compile(northpattern, re.IGNORECASE)
 southregex = re.compile(southpattern, re.IGNORECASE)
 #Loading Routes
-for row in cr:
-	newroute = BusRoute()
-	newroute.route_id = row[0]
-	newroute.route_short_name = row[1]
-	newroute.save()
 
 #loading Stops
 
@@ -39,12 +32,12 @@ north = Direction.objects.get(name="North")
 south = Direction.objects.get(name="South")
 nonedirection = Direction.objects.get(name="None")
 for row in cr2:
-	findEast = eastregex.match(row[1])
-	findWest = westregex.match(row[1])
-	findNorth = northregex.match(row[1])
-	findSouth = southregex.match(row[1])
-	route = BusRoute.objects.get(route_id=row[0])
-	stop_name = row[1]
+	findEast = eastregex.match(row[0])
+	findWest = westregex.match(row[0])
+	findNorth = northregex.match(row[0])
+	findSouth = southregex.match(row[0])
+	route = BusRoute.objects.get(route_id="26-Victoria")
+	stop_name = row[0]
 	try:
 		newstop = BusStop.objects.get(stop_name=stop_name)
 		newstop.routes.add(route)
@@ -62,8 +55,8 @@ for row in cr2:
 		newstop = BusStop()
 		newstop.stop_name = stop_name
 		newstop.phone_number = "+15551234567"
-		newstop.lat=row[2]
-		newstop.lon=row[3]
+		newstop.lat=row[1]
+		newstop.lon=row[2]
 		if findEast:
 			newstop.direction = east
 		elif findWest:
