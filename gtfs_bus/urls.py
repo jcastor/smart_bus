062891b92fde18ba2b2097ed4c22aa0390d2df30
Jason Castor <jason@jason-castor.com>
2013-03-04 17:28:00 -0800

    added serializers and api call toget bus locations given a route

--- gtfs_bus/urls.py
+++ gtfs_bus/urls.py
@@ -7,4 +7,5 @@ from gtfs_bus import views
 urlpatterns = patterns('gtfs_bus.views',
 	url(r'^$', TemplateView.as_view(template_name="gtfs_bus/default.html"), name="main"),
 	url(r'^routes/', ListView.as_view(model=Route, context_object_name="route_list",), name="route_list"),
+	url(r'^rest/buslocations/(?P<route>\w+)/$', views.BusDetail.as_view()),
 )
--- gtfs_bus/views.py
+++ gtfs_bus/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView, TemplateView
 from django.views.decorators.cache import cache_page
 from django.db.models import Q
 from gtfs_bus.models import *
+from gtfs_bus.serializers import BusSerializer
 from django import forms
 from gmapi import maps
 from gmapi.forms.widgets import GoogleMap
@@ -16,4 +17,20 @@ class MapForm(forms.Form):
 	map = forms.Field(widget=GoogleMap(attrs={'width':510, 'height':510}))
 
 def displayroute(request, pk):
-	route = Route.objects.get(id=pk) 
+	route = Route.objects.get(id=pk)
+
+class BusDetail(generics.RetrieveAPIView):
+	model = Bus
+	serializer_class = BusSerializer
+	def get(self, request, route, format=None):
+		bus_list = []
+		our_route = Route.objects.get(route_shortname = route)
+		our_trips = Trip.objects.filter(route = our_route)
+		for trips in our_trips:
+			try:
+				bus_list += Bus.objects.filter(trip = trips)
+			except:
+				pass
+		serializer = BusSerializer(bus_list)
+		return Response(serializer.data)
+ 
