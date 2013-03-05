571d09d903839b384d25e5e205900560283d7f2c
Jason Castor <jason@jason-castor.com>
2013-03-05 09:17:56 -0800

    added bus update via sms/GET

--- gtfs_bus/views.py
+++ gtfs_bus/views.py
@@ -13,10 +13,28 @@ from rest_framework import generics
 from rest_framework.response import Response
 import math
 
+def update_location(request):
+	textmessage = request.GET.get('Body', '')
+	sender = request.GET.get('From', '')
+	#http://499.jason-castor.com/gtfs/update/?From=%2B12502086479&Body=U%3B48.1%3B-123.1
+	if textmessage:
+		if textmessage.startswith("U"): #message should be of the form U;lat;lon
+			split = textmessage.split(';')
+			bus = Bus.objects.get(phone_num = sender)
+			bus.lat = split[1]
+			bus.lon = split[2]
+			bus.save()
+			return render_to_response("gtfs_bus/success.html", RequestContext(request))
+		else:
+			if sender != "+911":
+				twilio_client.sms.messages.create(to=sender, from_="+12509842369", body="")	
+	
+	return render_to_response("gtfs_bus/default.html", RequestContext(request))
+
 class MapForm(forms.Form):
 	map = forms.Field(widget=GoogleMap(attrs={'width':510, 'height':510}))
 
-def displayroute(request, pk):
+def display_route(request, pk):
 	route = Route.objects.get(id=pk)
 
 class BusDetail(generics.RetrieveAPIView):
