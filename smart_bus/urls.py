141936235473363be3441ea469e55b3ebe3b161c
Jason Castor <jason@jason-castor.com>
2013-04-25 10:38:49 -0700

    General Cleanup, removed old application that was deprecated

--- smart_bus/urls.py
+++ smart_bus/urls.py
@@ -15,7 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
 	url(r'^$', include('gtfs_bus.urls')),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^sbs/', include('bus_tracker.urls')),
 	url(r'^gtfs/', include('gtfs_bus.urls')),
 
 )
